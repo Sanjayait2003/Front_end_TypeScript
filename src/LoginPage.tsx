39c446e18b80b60ba50f9d1cc597868e4dc10b78
Sanjayait2003 <sanjayrait13@gmail.com>
2025-03-21 11:49:14 +0530

    added logs

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -14,6 +14,9 @@ const LoginPage = () => {
   interface LoginResponse {
     token: string;
   }
+
+  console.log('Test');
+  
   
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault(); 
